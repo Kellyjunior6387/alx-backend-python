568922b2ef7d6c31e6fc3111ef112d388aef7b5c
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-08 23:29:51 +0300

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Function that is similar to wait_n but uses regular"""
+"""Function that is similar to wait_n but uses regular.
+"""
 from typing import List
 import asyncio
 
@@ -8,8 +9,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-    Function that calls task_wait_random and run n times
+    """Function that calls task_wait_random and run n times.
     """
     delays = await asyncio.gather(*(task_wait_random(max_delay)
                                     for _ in range(n)))
