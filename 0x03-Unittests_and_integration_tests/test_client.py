a539b835bbfc59ac6df2b28e12549f336d11b97e
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-29 12:25:42 +0300

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -27,3 +27,22 @@ class TestGithubOrgClient(unittest.TestCase):
         mock_org.return_value = {'repos_url': expected}
         client = GithubOrgClient('test-org')
         self.assertEqual(client._public_repos_url, expected)
+
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """Test the public_repos method"""
+        expected_repos_url = 'https://api.github.com/orgs/test-org/repos'
+        mock_public_repos_url.return_value = expected_repos_url
+        repos_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+        mock_get_json.return_value = repos_payload
+        client = GithubOrgClient('test-org')
+        repos = client.public_repos()
+        expected_repos = ["repo1", "repo2", "repo3"]
+        self.assertEqual(repos, expected_repos)
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(expected_repos_url)
