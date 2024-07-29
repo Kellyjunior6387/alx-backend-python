97749ce2711828092066adefb7827f145360b767
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-29 12:51:48 +0300

    task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,13 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(repos, expected_repos)
         mock_public_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with(expected_repos_url)
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test the has_license method"""
+        client = GithubOrgClient('test')
+        result = client.has_license(repo, license_key)
+        self.assertEqual(result, expected)
